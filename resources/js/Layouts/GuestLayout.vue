4fc7168a6ba5a471d871290e5fe7897d5ebb9a43
Juan Pablo Suarez Ramirez <86664381+juanp-suarezr@users.noreply.github.com>
2024-03-12 14:13:25 -0500

    actu

---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex h-screen items-center justify-center bg-gray-200 px-6">
+        <div class="w-full max-w-sm rounded-md bg-sky-800 p-6 shadow-md">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
