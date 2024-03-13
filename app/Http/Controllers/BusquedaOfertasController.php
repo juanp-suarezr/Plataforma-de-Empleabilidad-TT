b426a3f3b536745089e7204f36ba7d33d0c2d34e
Juan Pablo Suarez Ramirez <86664381+juanp-suarezr@users.noreply.github.com>
2024-03-13 13:20:58 -0500

    actu busquedaOfertasController

--- app/Http/Controllers/BusquedaOfertasController.php
+++ app/Http/Controllers/BusquedaOfertasController.php
@@ -13,22 +13,24 @@ class BusquedaOfertasController extends Controller
     function __construct()
     {
         $this->middleware('permission:buscarOfertas-list', ['only' => ['index', 'store']]);
-        
     }
 
-    function index() {
+    function index()
+    {
         return Inertia::render('BuscarOfertas/Index', [
 
             'ofertas' => Ofertas::query()
-                ->when(RequestFacade::input('search'),function($query, $search) {
-                    $query->where('ubicacion','like','%'.$search.'%')
-                    ->OrWhere('nombre_empresa','like','%'.$search.'%');
-                })->when(RequestFacade::input('estado_users'), function ($query, $estado) {
-                    $query->where('estado', $estado);
+                ->join('usuario', 'ofertas.empresa_id', '=', 'usuario.id')
+                ->select('ofertas.*', 'usuario.nombre', 'usuario.logo', 'usuario.nit', 'usuario.telefono')
+                ->when(RequestFacade::input('search'), function ($query, $search) {
+                    $query->where('ubicacion', 'like', '%' . $search . '%')
+                        ->OrWhere('nombre', 'like', '%' . $search . '%');
+                })->when(RequestFacade::input('jornada'), function ($query, $jornada) {
+                    $query->where('jornada', $jornada);
                 })->with('roles')->paginate(5)
                 ->withQueryString(),
-                'filters' => RequestFacade::only(['search'])
-            
+            'filters' => RequestFacade::only(['search'])
+
         ]);
     }
 }
